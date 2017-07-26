d5f357e7ab924adb0f06568da9c9fff792dc566e
Morten Broesby-Olsen <levorto@gmail.com>
2017-07-26 21:05:17 +0200

    Added canvas player

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Empty for now...
+var canvasVideo = new CanvasVideoPlayer({
+  videoSelector: '.video',
+  canvasSelector: '.canvas',
+  timelineSelector: false,
+  autoplay: true,
+  makeLoop: true,
+  pauseOnClick: false,
+  audio: false
+});
+
+/* Below might be useful later...
+
+var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+if (!isIOS) {
+  var canvasVideo = new CanvasVideoPlayer({
+    videoSelector: '.video',
+    canvasSelector: '.canvas',
+    timelineSelector: false,
+    autoplay: true,
+    makeLoop: true,
+    pauseOnClick: false,
+    audio: false
+  });
+} else {
+  document.querySelectorAll('.canvas')[0].style.display = 'none'; // Use HTML5 video
+}
+*/
