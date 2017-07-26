8c893aa03ee3ad4aa59aedd1695249dcc43fe1bf
Morten Broesby-Olsen <levorto@gmail.com>
2017-07-26 21:09:13 +0200

    Added back non-IOS native player

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-var canvasVideo = new CanvasVideoPlayer({
-  videoSelector: '.video',
-  canvasSelector: '.canvas',
-  timelineSelector: false,
-  autoplay: true,
-  makeLoop: true,
-  pauseOnClick: false,
-  audio: false
-});
-
-/* Below might be useful later...
-
 var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
 
-if (!isIOS) {
+if (isIOS) {
   var canvasVideo = new CanvasVideoPlayer({
     videoSelector: '.video',
     canvasSelector: '.canvas',
@@ -25,4 +13,3 @@ if (!isIOS) {
 } else {
   document.querySelectorAll('.canvas')[0].style.display = 'none'; // Use HTML5 video
 }
-*/
